9a9ad0da933097a5af161dfe132d935f88abc37d
isaac c <isaacgc0596@gmail.com>
2020-02-10 20:15:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   final_hash = {}
   data.each do |key, value|
     value.each do |values, names|
       names.each do |name|
-        if final_hash[name] == final_hash
-          final_hash[name]
+        if !final_hash[name]
+          final_hash[name] = {}
         end
+        if !final_hash[name][key]
+          final_hash[name][key] = []
+        end
+        final_hash[name][key] << values.to_s
       end
     end
   end
