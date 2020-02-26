4942b33f555feaa1bd768b06b287eddfe35d801a
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-26 05:09:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,20 +15,24 @@ end
 
 def nyc_pigeon_organizer(data)
   list = name_list(data)
-  new_list = {}
   data.each do |key,value|
     value.each do |k,v|
       list.each do |pigeon,data|
         arr = []
         if v.include?(pigeon)
-          p "I have found #{pigeon} in this array with this key:value"
-          pp k,v
-          p "The array they should be returning is this simplified"
-          pp arr << k.to_s
-          new_list[pigeon] = {key => arr}
+          if list[pigeon].key?(key) == false
+
+            arr << k.to_s
+            list[pigeon][key] = arr
+
+          else
+            list[pigeon][key] << k.to_s
+          end
+          pp list
         end
 
       end
     end
   end
+  list
 end
