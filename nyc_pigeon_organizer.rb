1ccda7f1755a54191b293e6680d237dda9616f98
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-26 03:36:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pp'
+
+def pigeon_hash(data)
+  list = {}
+  data.each do |key,value|
+    value.each do |k,v|
+      v.each do |name|
+        if list[name][key]
+          list[name][key] << k.to_s
+        else
+          list[name] = {key => [k.to_s]}
+        end
+        pp list
+      end
+    end
+  end
+end
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash(data)
 end
