ea1257733fefe657f5389d11c66352665e12a015
Ruth Obe <ruthobey@gmail.com>
2020-02-28 20:28:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = Hash.new
+  
+  #create an inner hash that takes color, gender, lives as keys and an array as value
+  inner_hash = Hash.new 
+  hash_names = data.keys
+  hash_names.each do |key|
+    inner_hash[key] = []
+  end 
+  
+  binding.pry
+  
+  data[:color].reduce do ||
+    
+    
+  end 
+  
+  pigeon_list
 end
